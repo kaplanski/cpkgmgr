722a01a0b5579f777a459ce1975a567929a4d1a3
Jan-Daniel Kaplanski <kaplanski@uni-potsdam.de>
2019-01-29 11:27:37 +0100

    minor changes

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#Â!usr/bin/env bash
+#!/usr/bin/env bash
 
 CC=
 PROG="cpkgmgr"
@@ -14,40 +14,48 @@ if [ "$PWD" == "$PROGPATH" -o "$PWD" == "/usr$PROGPATH" ]; then
    exec ./build.sh
 fi
 
-#set compiler
-echo "Setting compiler..."
-if [ -f /usr/bin/gcc -o -f /usr/local/bin/gcc ]; then
-   CC=gcc
-   echo "   using gcc"
-elif [ -f /usr/bin/clang -o -f /usr/local/bin/clang ]; then
-   CC=clang
-   echo "   using clang"
-else
-   echo "   Neither clang nor gcc was found on your system!"
-   exit 255
+if [ "$1" == "" -o "$1" == "all" ]; then
+   #set compiler
+   echo "Setting compiler..."
+   if [ -f /usr/bin/gcc -o -f /usr/local/bin/gcc ]; then
+      CC=gcc
+      echo "   using gcc"
+   elif [ -f /usr/bin/clang -o -f /usr/local/bin/clang ]; then
+      CC=clang
+      echo "   using clang"
+   else
+      echo "   Neither clang nor gcc was found on your system!"
+      exit 255
+   fi
+   echo "   DONE"
+
+   #dependency check
+   echo "Checking dependencies..."
+   if [ ! -f /usr/bin/wget -a ! -f /usr/local/bin/wget ]; then
+      echo "   wget was not found on your system"
+      echo "   Please install wget to use $PROG!"
+      exit 177
+   fi
+   echo "   DONE"
+
+   #it's showtime
+   echo
+   echo "$CC $FLAGS $PROG.c -o $PROG"
+   $CC $FLAGS $PROG.c -o $PROG
+   echo
+
+elif [ "$1" == "install" -a -f $PROG ]; then
+   if [ ! -f $PROGPATH/$PROG ];then
+      #initial execution (creates folders and such)
+      echo "First execution"
+      echo
+      ./$PROG "-h"
+      echo
+   fi
+   echo "$PROG is copied to $PROGPATH"
+   cp $PROG $PROGPATH
+
+elif [ "$1" == "clean" ];then
+   rm -rf $PROG
 fi
-echo "   DONE"
-
-#dependency check
-echo "Checking dependencies..."
-if [ ! -f /usr/bin/wget -a ! -f /usr/local/bin/wget ]; then
-   echo "   wget was not found on your system"
-   echo "   Please install wget to use $PROG!"
-   exit 177
-fi
-echo "   DONE"
-
-#it's showtime
-echo
-echo "$CC $FLAGS $PROG.c -o $PROG"
-$CC $FLAGS $PROG.c -o $PROG
-echo
-
-#initial execution (creates folders and such)
-echo "First execution"
-echo
-./$PROG "-h"
-echo
-echo "$PROG is copied to $PROGPATH"
-cp $PROG $PROGPATH
 echo "All done, have fun!"
--- cpkgmgr.c
+++ cpkgmgr.c
@@ -4,6 +4,9 @@
  MIT/X11 LICENSE
 */
 
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
